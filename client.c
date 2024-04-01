09b598e00720e74ff848836ca7987867dd6a7c0e
Murilo Calegari <murilo478@gmail.com>
2024-04-01 15:55:17 +0200

    Add header, translate client.c to English

--- client.c
+++ client.c
@@ -7,62 +7,60 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include "match.h"
 
-void genere_entete(uint16_t codereq, uint16_t id, uint16_t eq, uint16_t *entete){
-    *entete = (codereq << 3) | (id << 1) | eq;
-}
-
-int connexion(int port, char* addr){
-    //*** creation de la socket ***
-    int fdsock = socket(PF_INET6, SOCK_STREAM, 0);
-    if(fdsock == -1){
-        perror("creation socket");
+int connect_to_server(int port, char* addr){
+    //*** create socket ***
+    int sockfd = socket(PF_INET6, SOCK_STREAM, 0);
+    if(sockfd == -1){
+        perror("socket creation");
         return -1;
     }
 
-    //*** creation de l'adresse du destinataire (serveur) ***
-    struct sockaddr_in6 address_sock;
-    memset(&address_sock, 0,sizeof(address_sock));
-    address_sock.sin6_family = AF_INET6;
-    address_sock.sin6_port = htons(port);
-    inet_pton(AF_INET6, addr, &address_sock.sin6_addr);
+    //*** create server address ***
+    struct sockaddr_in6 server_address;
+    memset(&server_address, 0,sizeof(server_address));
+    server_address.sin6_family = AF_INET6;
+    server_address.sin6_port = htons(port);
+    inet_pton(AF_INET6, addr, &server_address.sin6_addr);
 
-    //*** demande de connexion au serveur ***
-    int r = connect(fdsock, (struct sockaddr *) &address_sock, sizeof(address_sock));
+    //*** connect to server ***
+    int r = connect(sockfd, (struct sockaddr *) &server_address, sizeof(server_address));
     if(r == -1){
-        perror("echec de la connexion");
-        close(fdsock);
+        perror("connection failed");
+        close(sockfd);
         return -1;
     }
 
-    return fdsock;
+    return sockfd;
 }
 
 
 int main(int argc, char** args){
-    int fdsock;
+    int sockfd;
     int port = atoi(args[1]);
     char* addr = args[2];
 
-    if((fdsock = connexion(port, addr)) < 0){
-        perror("connexion");
+    if(argc != 3){
+        printf("usage: %s <port> <address>\n", args[0]);
         return 1;
     }
 
-    printf("connection réussit\n");
-
-    uint16_t entete;
-    uint16_t codereq; // reste à voir comment mettre les valeurs dedans (peut etre avec le truc graphique)
-    uint16_t id; // reste à voir comment mettre les valeurs dedans (peut etre avec le truc graphique)
-    uint16_t eq; // reste à voir comment mettre les valeurs dedans (peut etre avec le truc graphique)
-    genere_entete(codereq,id,eq,&entete);
-
-    //*** envoie d'un message ***
-    int ecrit = send(fdsock, &entete, sizeof(uint16_t), 0);
-    if(ecrit <= 0){
-        perror("erreur ecriture");
-        exit(3);
+    if((sockfd = connect_to_server(port, addr)) < 0){
+        perror("connection");
+        return 1;
     }
+
+    printf("connection successful\n");
+
+    //*** send a message ***
+    // uint16_t header;
+    // generate_header(codereq, id, eq, &header);
+    // int written = send(sockfd, &header, sizeof(uint16_t), 0);
+    // if(written <= 0){
+    //     perror("write error");
+    //     exit(3);
+    // }
 
     return 0;
 }
